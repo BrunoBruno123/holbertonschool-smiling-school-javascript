d5a438f9297ce7ee201b3c7625f8e2bb8b6de00b
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-02-14 23:30:41 +0100

    changes

--- holbertonschool-smiling-school-javascript/src/App.jsx
+++ holbertonschool-smiling-school-javascript/src/App.jsx
@@ -1,17 +1,18 @@
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Courses from "./Pages/Courses";
 import Pricing from "./Pages/Pricing";
 import Home from "./Pages/Home";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Courses" element={<Courses />} />
+        <Route path="/Pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Courses" element={<Courses />} />
-      <Route path="/Pricing" element={<Pricing />} />
-    </Routes>
-  </BrowserRouter>
-);
+export default App;
